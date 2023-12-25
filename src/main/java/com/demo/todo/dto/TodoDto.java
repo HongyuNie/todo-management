2672f099cb07a3ade1cf8df74398df088bf4fc5d
hongyu <nhy0613@outlook.com>
2023-12-25 10:33:27 -0800

    create todo repo and todoDTO

---
+++ src/main/java/com/demo/todo/dto/TodoDto.java
@@ -0,0 +1,17 @@
+package com.demo.todo.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDto {
+    private Long id;
+    private String description;
+    private boolean completed;
+}
