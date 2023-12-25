929532ebbc46272db48fa5a1f45bb5f1ff7dd0fb
hongyu <nhy0613@outlook.com>
2023-12-25 14:30:41 -0800

    build rest api: create controller class and finish addtodo rest api.

---
+++ src/main/java/com/demo/todo/controller/TodoController.java
@@ -0,0 +1,23 @@
+package com.demo.todo.controller;
+
+import com.demo.todo.dto.TodoDto;
+import com.demo.todo.service.TodoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    // build add to do rest api
+    @PostMapping
+    public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto) {
+        TodoDto savedTodo = todoService.addTodo(todoDto);
+
+        return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
+    }
+}
