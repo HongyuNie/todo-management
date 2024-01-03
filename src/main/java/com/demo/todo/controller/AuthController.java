fd83200c9c5d0971e319fa3abeaccc541fb23942
hongyu <nhy0613@outlook.com>
2024-01-02 22:24:25 -0800

    add JWT related maven dependencies,create JwtAuthenticationEntryPoint class, add JWT properties in application.properties file.

--- src/main/java/com/demo/todo/controller/AuthController.java
+++ src/main/java/com/demo/todo/controller/AuthController.java
@@ -6,14 +6,12 @@ import com.demo.todo.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
     private AuthService authService;
 
