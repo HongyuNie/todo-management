d7b581a13737ed5508ba9acb99591f81449ec5a9
hongyu <nhy0613@outlook.com>
2024-01-04 09:42:07 -0800

    change login method in AuthServiceImpl class to return the role along with JWT token.

--- src/main/java/com/demo/todo/service/AuthService.java
+++ src/main/java/com/demo/todo/service/AuthService.java
@@ -1,9 +1,10 @@
 package com.demo.todo.service;
 
+import com.demo.todo.dto.JwtAuthResponse;
 import com.demo.todo.dto.LoginDto;
 import com.demo.todo.dto.RegisterDto;
 
 public interface AuthService {
     String register(RegisterDto registerDto);
-    String login(LoginDto loginDto);
+    JwtAuthResponse login(LoginDto loginDto);
 }
